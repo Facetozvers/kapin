b5daeffb0de45063dbaa1d54a509107640b2a7c5
Facetozvers <whatthefapmc@gmail.com>
2021-02-04 17:58:06 +0700

    tentang kapin

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,11 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">
     <div class="row">
-        <div class="col-md-3">
-            <img src="/assets/logo.jpg" height="100" width="100" alt="">
+        <div class="col-md-5">
+            <img src="/assets/logo.png" height="150" width="150" alt="">
         </div>
-        <div class="col-md-6 mt-4">
-            <p class="logo-text">Yayasan Pendidikan Teknologi<br>Kapin</p>
+        <div class="col-md-5 mt-1">
+            <p class="logo-text"><span class="header">KAPIN</span><br>YAYASAN PENDIDIKAN TEKNOLOGI<br>KADER PEMBANGUNAN INDONESIA</p>
         </div>
     </div>
     
@@ -17,19 +17,33 @@
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-    <ul class="navbar-nav ml-auto mt-2 mt-lg-0" style="border: 1px solid black; border-right: none">
-      <li class="nav-item active" style="border-right: 1px solid black">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active" style="border-right: 1px solid black">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item active" style="border-right: 1px solid black">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-    </ul>
-  </div>
+  <div class="collapse navbar-collapse flex-column" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Beranda</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Yayasan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Kegiatan</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav flex-row mb-2 ml-auto">
+                <li class="nav-item pr-1">
+                    <a class="nav-link secondary py-1 pr-4 pl-4 text-center" href="#">SMP</a>
+                </li>
+                <li class="nav-item pr-1">
+                    <a class="nav-link secondary py-1 pr-4 pl-4 text-center" href="#">SMA</i></a>
+                </li>
+                <li class="nav-item pr-1">
+                    <a class="nav-link secondary py-1 pr-4 pl-4 text-center" href="#">SMK</i></a>
+                </li>
+            </ul>
+        </div>
 </nav>
 
 <div class="container-fluid hero-section">
@@ -38,32 +52,78 @@
   </div>
 </div>
 
-<div class=" container tingkat-section mt-5">
+<div class="container-fluid section tingkat-section mt-5">
+  <h1 class="section-title text-center">Akademik</h1>
   <div class="row justify-content-center">
     <div class="col-md-3">
-      <div class="card">
-        <img class="card-img-top" src="https://www.kalderanews.com/wp-content/uploads/2019/03/Siswa-SMP-MTS.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title text-center">SMP</h5>
+      <div class="tingkat-card card">
+        <img class="tingkat-img card-img" src="https://mtsn4malang.sch.id/wp-content/uploads/2019/08/placeholder-1.png" alt="Bologna">
+        <div class="card-img-overlay text-white d-flex flex-column justify-content-end" style="">
+          <div class="d-block tingkat-overlay">
+            <h1 class="tingkat-title card-title text-center my-3" style="margin:0">SMP</h1>
+          </div>
         </div>
       </div>
     </div>
     <div class="col-md-3">
-      <div class="card">
-        <img class="card-img-top" src="https://www.kalderanews.com/wp-content/uploads/2019/03/Siswa-SMP-MTS.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title text-center">SMA</h5>
+      <div class="tingkat-card card">
+        <img class="tingkat-img card-img" src="https://www.kalderanews.com/wp-content/uploads/2019/03/Siswa-SMP-MTS.jpg" alt="Bologna">
+        <div class="card-img-overlay text-white d-flex flex-column justify-content-end" style="">
+          <div class="d-block" style="position : absolute;bottom   : 0;left:0;width:100%;background-color: rgba(0, 0, 0, 0.4)">
+            <h1 class="tingkat-title card-title text-center my-3" style="margin:0">SMA</h1>
+          </div>
         </div>
       </div>
     </div>
     <div class="col-md-3">
-      <div class="card">
-        <img class="card-img-top" src="https://www.kalderanews.com/wp-content/uploads/2019/03/Siswa-SMP-MTS.jpg" alt="Card image cap">
-        <div class="card-body">
-          <h5 class="card-title text-center">SMK</h5>
+      <div class="tingkat-card card">
+        <img class="tingkat-img card-img" src="https://www.kalderanews.com/wp-content/uploads/2019/03/Siswa-SMP-MTS.jpg" alt="Bologna">
+        <div class="card-img-overlay text-white d-flex flex-column justify-content-end" style="">
+          <div class="d-block" style="position : absolute;bottom   : 0;left:0;width:100%;background-color: rgba(0, 0, 0, 0.4)">
+            <h1 class="tingkat-title card-title text-center my-3" style="margin:0">SMK</h1>
+          </div>
         </div>
       </div>
     </div>
   </div>
+</div>
+
+<div class="container-fluid section" style="background: #ff781f">
+  <h1 class="section-title section-title-white text-center">Tentang Kapin</h1>
+    <div class="row justify-content-center">
+      <div class="col-md-4 col-5">
+        <div class="card">
+          <img src="https://mtsn4malang.sch.id/wp-content/uploads/2019/08/placeholder-1.png" class="card-img-top" alt="...">
+        </div>
+      </div>
+      <div class="col-md-4 col-5">
+        <div class="card">
+          <img src="https://mtsn4malang.sch.id/wp-content/uploads/2019/08/placeholder-1.png" class="card-img-top" alt="...">
+        </div>
+      </div>
+    </div>
+
+    <div class="row about mt-5 " style="padding-left: 15vw;padding-right:15vw">
+      <h1 class="about-heading mb-3">Heading</h1>
+      <p class="about-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae sapien pellentesque habitant morbi. Sed velit dignissim sodales ut. Ultrices sagittis orci a scelerisque purus semper eget duis at. Lectus arcu bibendum at varius vel. Egestas quis ipsum suspendisse ultrices gravida dictum fusce. Sodales ut etiam sit amet nisl purus in mollis. Magna fermentum iaculis eu non diam phasellus. Ut sem viverra aliquet eget sit. Egestas egestas fringilla phasellus faucibus scelerisque eleifend donec pretium vulputate.</p>
+    </div>
+
+    <div class="row justify-content-center about-numbers">
+      <div class="col-md-3 col-12">
+        <div class="diamond-shape">
+          <div class="item-count">99</div>
+        </div>  
+      </div>
+      <div class="col-md-3 col-12">
+        <div class="diamond-shape">
+          <div class="item-count">99</div>
+        </div>  
+      </div>
+      <div class="col-md-3 col-12">
+        <div class="diamond-shape">
+          <div class="item-count">99</div>
+        </div>  
+      </div>
+    </div>
 </div>
 @endsection
