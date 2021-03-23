fac16e8d8375614ebe87386bba9c2aa79b92189e
Facetozvers <whatthefapmc@gmail.com>
2021-03-23 20:56:37 +0700

    kegiatan content update

--- resources/views/kegiatan.blade.php
+++ resources/views/kegiatan.blade.php
@@ -10,81 +10,40 @@
 </div>
 <div class="container-fluid section text-section" style="">
     <h1 class="section-title section-title-orange pt-3">KEGIATAN</h1>
-    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p class="mb-5">Untuk membangun suasana kegiatan belajar yang kondusif dan menyenangkan bagi  siswa, selain memberikan fasilitas yang mumpuni bagi kegiatan belajar mengajar di sekolah, Yayasan KAPIN juga memberikan variasi kegiatan yang menarik dan mampu menggali minat, bakat juga kreativitas siswa. Seperti kegiatan ekstrakulikuler, acara hari besar, class meeting dan mengikuti berbagai macam perlombaan.</p>
 
     <div class="row justify-content-center">
         <div class="col-md-4">
             <img class="img-kegiatan rounded mx-auto d-block" src="/assets/img/kegiatan/1.jpeg" style="" alt="">
-            <p class="img-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <p class="img-desc" style="font-size:0.82vw; line-height: 0.5cm">Menggali minat dan bakat anak lewat berbagai kegiatan menjadi salah satu upaya YPT KAPIN membangun kemampuan yang dimiliki siswanya. Siwa dapat memilih berbagai kegiatan ekstrakulikuler yang bermanfaat dan sesuai dengan minat juga bakatnya, seperti pramuka, tari, taekwondo, futsal dan masih banyak lagi.</p>
         </div>
         <div class="col-md-4">
             <img class="img-kegiatan rounded mx-auto d-block " src="/assets/img/kegiatan/2.jpeg" style="" alt="">
-            <p class="img-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <p class="img-desc" style="font-size:0.82vw; line-height: 0.5cm">Hobi dan minat siswa harus selalu didukung penuh dan dikembangkan. Seperti yang dilakukan YPT KAPIN dalam mendukung bakat dan minat siswa dalam seni tari, memberikan pelatihan dan mengikutsertakannya dalam berbagai kompetisi.  Lewat berbagai pengalaman berkompetisi, siswa akan mendapatkan banyak  pembelajaran dan pengalaman yang membangun karakter.</p>
         </div>
         <div class="col-md-4">
             <img class="img-kegiatan rounded mx-auto d-block " src="/assets/img/kegiatan/3.jpeg" style="" alt="">
-            <p class="img-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
-        </div>
-    </div>
-
-    <div class="row justify-content-center">
-        <div class="w-100 d-block pb-3">
-            <div class="kegiatan-button px-2 py-2">
-                <button class="button-no-outline navbar-toggler w-100 text-left" type="button" data-toggle="collapse" data-target="#TogglerSMP" aria-controls="TogglerSMP" aria-expanded="false" aria-label="Toggle navigation">
-                + KEGIATAN SMP
-                </button>
-            </div>
-            <div class="kegiatan-button-content collapse" id="TogglerSMP">
-                <div class=" p-4">
-                    <h5 class="text-dark h4">Collapsed content</h5>
-                    <span class="text-muted">Toggleable via the navbar brand.</span>
-                </div>
-            </div>
-        </div>
-        <div class="w-100 d-block pb-3">
-            <div class="kegiatan-button px-2 py-2">
-                <button class="button-no-outline navbar-toggler w-100 text-left" type="button" data-toggle="collapse" data-target="#TogglerSMA" aria-controls="TogglerSMA" aria-expanded="false" aria-label="Toggle navigation">
-                + KEGIATAN SMA
-                </button>
-            </div>
-            <div class="kegiatan-button-content collapse" id="TogglerSMA">
-                <div class=" p-4">
-                    <h5 class="text-dark h4">Collapsed content</h5>
-                    <span class="text-muted">Toggleable via the navbar brand.</span>
-                </div>
-            </div>
-        </div>
-        <div class="w-100 d-block pb-3">
-            <div class="kegiatan-button px-2 py-2">
-                <button class="button-no-outline navbar-toggler w-100 text-left" type="button" data-toggle="collapse" data-target="#TogglerSMK" aria-controls="TogglerSMK" aria-expanded="false" aria-label="Toggle navigation">
-                + KEGIATAN SMK
-                </button>
-            </div>
-            <div class="kegiatan-button-content collapse" id="TogglerSMK">
-                <div class=" p-4">
-                    <h5 class="text-dark h4">Collapsed content</h5>
-                    <span class="text-muted">Toggleable via the navbar brand.</span>
-                </div>
-            </div>
+            <p class="img-desc" style="font-size:0.82vw; line-height: 0.5cm">Membangun semangat berkompetisi siswa sangatlah penting. Lewat bakat dan minat siswa terhadap cabang olahraga Karate, YPT KAPIN memberi dukungan dengan mengikutsertakan siswa dalam berbagai kompetisi dibidangnya. Membangun bakat yang terpendam menjadi prestasi yang gemilang. </p>
         </div>
     </div>
 </div>
 <div class="container-fluid section text-section" style="">
     <h1 class="section-title section-title-orange pt-4">EVENTS</h1>
-    <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p class="mb-5">Membangun karakter siswa paling mudah dilakukan dengan mengaplikasikannya lewat berbagai kegiatan yang menyenangkan dan menarik bagi siswa untuk berpartisipasi. Seperti membangun rasa peduli terhadap sesama lewat Baksos, membangun rasa cinta tanah air dengan memperingati hari-hari besar Nasional seperti Hari Kemerdekaan Indonesia, hari pahlawan, dan hari Kartini. Juga meningkatkan kebersamaan dan kekompakan dengan kegiatan pramuka, juga aneka perlombaan.</p>
 
     <div class="row justify-content-center">
         <div class="col-md-4">
             <img class="img-kegiatan rounded mx-auto d-block w-100" src="/assets/img/kegiatan/4.jpeg" style="" alt="">
-            <p class="img-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <p class="img-desc" style="font-size:0.82vw; line-height: 0.5cm">Memunculkan kepedulian siswa terhadap lingkungan sekitarnya, dilakukan YPT KAPIN dengan membiasakan siswa berbagi kepada orang-orang yang lebih membutuhkan lewat Baksos.</p>
         </div>
         <div class="col-md-4">
             <img class="img-kegiatan rounded mx-auto d-block w-100" src="/assets/img/kegiatan/5.jpeg" style="" alt="">
-            <p class="img-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <p class="img-desc" style="font-size:0.82vw; line-height: 0.5cm">Momen Hari Kemerdekaan menjadi hari penting yang diperingati oleh hampir seluruh rakyat Indonesia. YPT KAPIN selalu menyiapkan momen ini dengan baik demi membangkitkan semangat Nasionalisme siswa lewat hari-hari besar Nasional, salah satunya Hari Kemerdekaan Indonesia.</p>
         </div>
         <div class="col-md-4">
             <img class="img-kegiatan rounded mx-auto d-block w-100" src="/assets/img/kegiatan/6.jpeg" style="" alt="">
-            <p class="img-desc">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <p class="img-desc" style="font-size:0.82vw; line-height: 0.5cm">Jika bisa menyalurkan bakat sekaligus melestarikan budaya Indonesia kenapa tidak?
+YPT KAPIN selalu mendukung minat dan bakat positif siswa, juga melatihnya untuk berani tampil di depan teman-teman maupun dalam kompetisi.</p>
         </div>
     </div>
 </div>
